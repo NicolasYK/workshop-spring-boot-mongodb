fcf963aa4e265f7106c65389455e5bbe626139f3
NicolasYK <kosaka.nicolas@gmail.com>
2024-07-31 17:37:10 -0300

    Primeiro commit - Criando o projeto

---
+++ src/test/java/com/yuzo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
