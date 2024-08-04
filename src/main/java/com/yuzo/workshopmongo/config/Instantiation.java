0382013b7b1a4b4d3f1c24931f1de2bb166c85ee
NicolasYK <kosaka.nicolas@gmail.com>
2024-08-04 15:30:38 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/yuzo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/yuzo/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.yuzo.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.yuzo.workshopmongo.domain.Post;
 import com.yuzo.workshopmongo.domain.User;
+import com.yuzo.workshopmongo.repository.PostRepository;
 import com.yuzo.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,13 +19,26 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository user_repo;
 	
+	@Autowired
+	private PostRepository post_repo;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		user_repo.deleteAll();
+		post_repo.deleteAll();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("04/05/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraço", maria);
+		Post post2 = new Post(null, sdf.parse("06/03/2024"), "Bom dia!", "Acordei feliz hoje!", maria);
+		
+		
 		user_repo.saveAll(Arrays.asList(maria, alex, bob));
+		post_repo.saveAll(Arrays.asList(post1, post2));
 	}
 }
--- src/main/java/com/yuzo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/yuzo/workshopmongo/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.yuzo.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.yuzo.workshopmongo.domain.User;
+import com.yuzo.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
-	
+public interface PostRepository extends MongoRepository<Post, String>{
+
 }
